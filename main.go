987bb077145ce66b044fb3d864b6451b45f3352b
oat9002 <oat9002@hotmail.com>
2021-09-05 01:30:15 +0700

    :construction:

--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+
+	masterChef "github.com/oat9002/auto-compound/contracts"
 )
 
 func main() {
-	pancakeMainStakingAddress := "0x73feaa1eE314F8c655E354234017bE2193C9E24E"
+	myAddress := common.HexToAddress("0xf07dfFaFA1a9E49345f919040b1cbd6e4dDd5706")
+	pancakeMainStakingAddress := common.HexToAddress("0x73feaa1eE314F8c655E354234017bE2193C9E24E")
+
 	client, err := ethclient.Dial("https://bsc-dataseed.binance.org/")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	blockNumber := big.NewInt(int64(1))
+	instanceContract, err := masterChef.NewMasterChef(pancakeMainStakingAddress, client)
+	cakePool := big.NewInt(int64(0))
 
-	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	pendingCake, err := instanceContract.PendingCake(cakePool, myAddress)
 
-	fmt.Println(pancakeMainStakingAddress)
-	fmt.Println(block.Body())
+	fmt.Println(pendingCake)
 }
