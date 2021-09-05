95dc87009715094228180da1f3d5c953e65e55f1
oat9002 <oat9002@hotmail.com>
2021-09-05 22:01:03 +0700

    :recycle:

--- main.go
+++ main.go
@@ -15,24 +15,47 @@ import (
 
 func main() {
 	myAddress := common.HexToAddress("0xf07dfFaFA1a9E49345f919040b1cbd6e4dDd5706")
-	pancakeMainStakingAddress := common.HexToAddress("0x73feaa1eE314F8c655E354234017bE2193C9E24E")
-	callOpts := &bind.CallOpts{Pending: false, From: myAddress, BlockNumber: nil, Context: nil}
-	cakePool := big.NewInt(int64(0)) // Sylup pool
 
-	client, err := ethclient.Dial("https://bsc-dataseed.binance.org/")
+	client, err := GetClient("https://bsc-dataseed.binance.org/")
 
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
-	instanceContract, err := masterChef.NewMasterChef(pancakeMainStakingAddress, client)
+	contract, err := GetMasterChefContract(client)
 
-	pendingCake, err := instanceContract.PendingCake(callOpts, cakePool, myAddress)
+	pendingCake, err := GetPendingCakeFromSylupPool(contract, myAddress)
 
-	fmt.Println(fromWei(pendingCake))
+	fmt.Println(pendingCake)
 }
 
-func fromWei(data *big.Int) float64 {
+func GetClient(networkUrl string) (*ethclient.Client, error) {
+	return ethclient.Dial(networkUrl)
+}
+
+func GetMasterChefContract(client *ethclient.Client) (*masterChef.MasterChef, error) {
+	pancakeMainStakingAddress := common.HexToAddress("0x73feaa1eE314F8c655E354234017bE2193C9E24E")
+
+	return masterChef.NewMasterChef(pancakeMainStakingAddress, client)
+}
+
+func GetPendingCakeFromSylupPool(contract *masterChef.MasterChef, address common.Address) (float64, error) {
+	cakePool := big.NewInt(int64(0)) // Sylup pool
+	pendingCake, err := contract.PendingCake(GetDefaultCallOpts(address), cakePool, address)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return FromWei(pendingCake), nil
+}
+
+func GetDefaultCallOpts(address common.Address) *bind.CallOpts {
+	return &bind.CallOpts{Pending: false, From: address, BlockNumber: nil, Context: nil}
+}
+
+func FromWei(data *big.Int) float64 {
 	toReturn := float64(data.Int64()) / math.Pow10(18)
 
 	return math.Round(toReturn*math.Pow10(6)) / math.Pow10(6)
