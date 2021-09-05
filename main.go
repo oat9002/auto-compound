f3ff8a60c2c4999baa60267affc5d67f9065d81f
oat9002 <oat9002@hotmail.com>
2021-09-05 21:40:36 +0700

    :construction: able to get pending cake

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 
@@ -14,6 +16,8 @@ import (
 func main() {
 	myAddress := common.HexToAddress("0xf07dfFaFA1a9E49345f919040b1cbd6e4dDd5706")
 	pancakeMainStakingAddress := common.HexToAddress("0x73feaa1eE314F8c655E354234017bE2193C9E24E")
+	callOpts := &bind.CallOpts{Pending: false, From: myAddress, BlockNumber: nil, Context: nil}
+	cakePool := big.NewInt(int64(0)) // Sylup pool
 
 	client, err := ethclient.Dial("https://bsc-dataseed.binance.org/")
 
@@ -22,9 +26,14 @@ func main() {
 	}
 
 	instanceContract, err := masterChef.NewMasterChef(pancakeMainStakingAddress, client)
-	cakePool := big.NewInt(int64(0))
 
-	pendingCake, err := instanceContract.PendingCake(cakePool, myAddress)
+	pendingCake, err := instanceContract.PendingCake(callOpts, cakePool, myAddress)
 
-	fmt.Println(pendingCake)
+	fmt.Println(fromWei(pendingCake))
+}
+
+func fromWei(data *big.Int) float64 {
+	toReturn := float64(data.Int64()) / math.Pow10(18)
+
+	return math.Round(toReturn*math.Pow10(6)) / math.Pow10(6)
 }
