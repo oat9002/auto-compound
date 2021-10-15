cbb0495b70efbc53bf96dbb071b79da265d35281
oat9002 <oat9002@hotmail.com>
2021-10-16 01:32:57 +0700

    :recycle:

--- build-executable.sh
+++ build-executable.sh
@@ -2,14 +2,18 @@
 
 echo 'Build linux amd 64'
 env GOOS=linux GOARCH=amd64 go build
+chmod +x ./auto-compound
 
 echo 'Build linux arm 32'
 env GOOS=linux GOARCH=arm go build -o auto-compound-arm32
+chmod +x ./auto-compound-arm32
 
 echo 'Build linux arm 64'
 env GOOS=linux GOARCH=arm64 go build -o auto-compound-arm64
+chmod +x ./auto-compound-arm64
 
 echo 'Build windows amd 64'
 env GOOS=windows GOARCH=amd64 go build
+chmod +x ./auto-compound.exe
 
 
